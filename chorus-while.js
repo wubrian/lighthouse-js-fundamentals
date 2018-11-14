9efc6a7a26416264bc8512b26825b60d1742704d
Brian Wu <wubrian1234@hotmail.com>
2018-11-14 22:33:27 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,18 @@
+/* var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!"); */
+
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
